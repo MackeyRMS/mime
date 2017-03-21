185d47dac5b591f4b4b5231f96cd248c1f58de8c
kanishka <kanishka@mackeycorp.com>
2017-03-21 15:44:52 -0400

    Multipart with a plain text body and text attach test

--- test/unit/Codec/MIME/ParseTests.hs
+++ test/unit/Codec/MIME/ParseTests.hs
@@ -16,9 +16,15 @@ tests =
       [ testCase "examples" (do
           runParseEmail singlePartPlain @?= Just "This is the body text\r\n\r\n")
       , testCase "typical" (do
-          pure ())
+          runParseEmail' multiPartTwoPlain 
+            @?= Just ("this is the body\r\n","this is an attachment\n"))
       , testCase "exceptions" (do
           pure ())
+{-
+      , testCase "ics" (do
+          runParseEmail ""
+            @?= Nothing)
+-}
       ]
   ]
 
@@ -28,3 +34,13 @@ runParseEmail m =
       hs = MMT.mime_val_headers mv  -- to, from, subject
       content@(MMT.Single cont) = MMT.mime_val_content mv
   in Just cont
+
+runParseEmail' :: T.Text -> Maybe (T.Text, T.Text)
+runParseEmail' m =
+  let mv = MMP.parseMIMEMessage m
+      hs = MMT.mime_val_headers mv  -- to, from, subject
+      multi@(MMT.Multi mvs) = MMT.mime_val_content mv
+      mv1:mv2:_ = mvs
+      msg1@(MMT.Single cont) = MMT.mime_val_content mv1
+      msg2@(MMT.Single cont2) = MMT.mime_val_content mv2
+  in Just (cont, cont2)
